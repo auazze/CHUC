af2efcfb56a5fc75505b83fb1718ddfc4e4c9fe2
auazze <39782055+auazze@users.noreply.github.com>
2023-11-05 02:27:17 +0500

    Add files via upload

---
+++ TimeofDayProgram/TimeofDayProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class TimeOfDayProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите время в формате 24-часового времени (например, 14:30): ");
+        string input = Console.ReadLine();
+
+        if (TimeSpan.TryParse(input, out TimeSpan time))
+        {
+            if (time.Hours >= 6 && time.Hours < 12)
+            {
+                Console.WriteLine("Доброе утро!");
+            }
+            else if (time.Hours >= 12 && time.Hours < 18)
+            {
+                Console.WriteLine("Добрый день!");
+            }
+            else if (time.Hours >= 18 && time.Hours < 22)
+            {
+                Console.WriteLine("Добрый вечер!");
+            }
+            else
+            {
+                Console.WriteLine("Спокойной ночи!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введено некорректное время. Попробуйте еще раз.");
+        }
+
+        // Задержка консоли
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
