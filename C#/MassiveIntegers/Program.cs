c9d5a8386b2c9f4cad9b5f5921a23fa830c35fd6
auazze <39782055+auazze@users.noreply.github.com>
2023-12-07 22:20:52 +0500

    Add files via upload

---
+++ C#/MassiveIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] array =  new int[5];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 15000);
+        Console.WriteLine($"{i + 1}th element: {array[i]}");
+}
+
+Console.WriteLine("__________________________________________________");
+
+// или так
+for (int keys = 0; keys < array.Length; keys++)
+{
+    Console.WriteLine($"{keys + 1}th element: {array[keys]}");
+}
+
+Console.WriteLine("__________________________________________________");
+
+int up = 2;
+for (int j = 0; j < array.Length; j++)
+{
+    array[j] *= up;
+    Console.WriteLine($"Elevated: {array[j]}");
+}
+
+Console.WriteLine("__________________________________________________");
+
+for (int q = array.Length - 1; q >= 0; q--)
+{
+    Console.WriteLine($"Reverse: {array[q]}");
+}
+
+Console.WriteLine("__________________________________________________");
+
+int max = array.Max();
+Console.WriteLine($"Max number is: {max}");
