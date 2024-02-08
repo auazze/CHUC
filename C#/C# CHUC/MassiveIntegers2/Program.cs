9645de0753a95f65f6cc78ec84f482dd2f697915
auazze <39782055+auazze@users.noreply.github.com>
2024-02-08 22:16:24 +0500

    Add files via upload

---
+++ C#/C# CHUC/MassiveIntegers2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MassiveIntegers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Random random = new Random();
+            Console.WriteLine("Random: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 5);
+                Console.Write($"{array[i]}");
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Random reverse: ");
+            for (int q = array.Length - 1; q >= 0; q--)
+            {
+                Console.Write($"{array[q]}");
+            }
+            Console.WriteLine("\n");
+            Array.Sort(array);
+            Console.WriteLine("Sorted: ");
+            for (int j = array.Length - 1; j >= 0; j--)
+            {
+                Console.Write($"{array[j]}");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Sorted reverse: ");
+            for (int sr = 0; sr < array.Length; sr++)
+            {
+
+                Console.Write($"{array[sr]}");
+            }            
+        }
+    }
+}
